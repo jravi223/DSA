714e774549906c02eaf00f344832df25852768fb
Ravi <jravi223@gmail.com>
2023-04-13 00:11:53 +0530

    added palindromw file too

--- Day-1/palindrome.js
+++ Day-1/palindrome.js
@@ -1,1 +1,21 @@
-// hello
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    const str = x.toString();
+
+    let leftInd = 0;
+    let rightInd = str.length - 1;
+
+    while (leftInd < rightInd) {
+        if(str.charAt(leftInd) !== str.charAt(rightInd)) {
+            return false;
+        }
+        leftInd++;
+        rightInd--
+    }
+    return true;
+}
+
+console.log(isPalindrome(3123))
